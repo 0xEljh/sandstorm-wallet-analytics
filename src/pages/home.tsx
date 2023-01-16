a05e176deec3d733f09857ce676d7f1e508f9775
Elijah Ng <34589654+0xEljh@users.noreply.github.com>
2023-01-16 19:49:40 +0800

    add theme, routing. update readme.

---
+++ src/pages/home.tsx
@@ -0,0 +1,18 @@
+import { Flex, Stack, Heading, Text, Button } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex>
+      <Stack>
+        <Heading>Wallet Analytics</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          maiores nemo ut molestias ab modi corporis quae natus reprehenderit,
+          aperiam tenetur reiciendis dignissimos veniam fugit iusto nulla
+          debitis vitae nesciunt!
+        </Text>
+        <Button>CTA</Button>
+      </Stack>
+    </Flex>
+  );
+}
---
+++ src/themes/brandTheme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      0: "#FFFFFF",
+      100: "#37E6AF",
+      200: "#3B7080",
+      300: "#A23B50",
+      400: "#000000",
+    },
+  },
+});
