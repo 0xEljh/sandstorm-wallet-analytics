38eec38ab4855b74522c1c4b19ca4e42f1313a9e
Elijah Ng <34589654+0xEljh@users.noreply.github.com>
2023-01-31 12:58:42 +0800

    update home with routing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,21 @@
 import { Flex, Stack, Heading, Text, Button } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-function TextCTAPanel({ children }: { children: React.ReactNode }) {
+function TextCTAPanel({
+  link,
+  children,
+}: {
+  link: string;
+  children: React.ReactNode;
+}) {
+  // button should link to /internal-link
   return (
     <Flex direction="column" alignItems="center" minW={"100vw"}>
       <Text my={8}>{children}</Text>
-      <Button my={8}>Get Started</Button>
+      <Link to={link}>
+        <Button my={8}>Get Started</Button>
+      </Link>
     </Flex>
   );
 }
@@ -29,26 +39,26 @@ export default function Home() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <TextCTAPanel>
+            <TextCTAPanel link="/profile">
               View trading history and statistics. Track your performance and
               identify patterns in trading behavior. Create or join a trading
-              liquidity vault for on-chain social trading.
+              liquidity vault for on-chain social trading (Coming soon).
             </TextCTAPanel>
           </TabPanel>
           <TabPanel>
-            <TextCTAPanel>
+            <TextCTAPanel link="/profile">
               View your hodls, collection value, and performance. Showcase your
               ability to curate and collect NFTs.
             </TextCTAPanel>
           </TabPanel>
           <TabPanel>
-            <TextCTAPanel>
+            <TextCTAPanel link="/">
               View and apply for NFT whitelists. Make your case to NFT projects.
               Showcase your ability as a collector, trader, or both.
             </TextCTAPanel>
           </TabPanel>
           <TabPanel>
-            <TextCTAPanel>
+            <TextCTAPanel link="/creator">
               Curate a whitelist of applicants based on your desired holder
               profile. Choose applicants that will be the most valuable to your
               project.
