759437920c1ab323eed3b3e7728fd71642830d7c
Elijah Ng <34589654+0xEljh@users.noreply.github.com>
2023-02-01 16:25:13 +0800

    update creator page

--- src/pages/creator.tsx
+++ src/pages/creator.tsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
 
-import { Input, Button, Stack } from "@chakra-ui/react";
+import {
+  Input,
+  Button,
+  Select,
+  Stack,
+  Heading,
+  Text,
+  IconButton,
+  Icon,
+} from "@chakra-ui/react";
+import { MdDelete } from "react-icons/md";
 import { Tr, Td } from "@chakra-ui/react";
 import DataTable from "../components/dataTable";
+
 interface Address {
   address: string;
   status: string;
@@ -11,6 +22,7 @@ interface Address {
 export default function Creator() {
   const [addresses, setAddresses] = useState<Address[]>([]);
   const [newAddress, setNewAddress] = useState<string>("");
+  const [hasAddressError, setHasAddressError] = useState<boolean>(false);
 
   useEffect(() => {
     const storedAddresses = localStorage.getItem("addresses");
@@ -50,30 +62,69 @@ export default function Creator() {
     link.click();
   };
 
-  const handleAddAdress = () => {
+  const handleAddAddress = () => {
+    if (newAddress.length < 32) {
+      setHasAddressError(true);
+      return;
+    }
     setAddresses([...addresses, { address: newAddress, status: "" }]);
     setNewAddress("");
+    setHasAddressError(false);
+  };
+
+  const handleDeleteAddress = (index: number) => {
+    setAddresses(addresses.filter((_, i) => i !== index));
+  };
+
+  const handleUpdateStatus = (index: number, status: string) => {
+    const updatedAddresses = [...addresses];
+    updatedAddresses[index].status = status;
+    setAddresses(updatedAddresses);
   };
 
   return (
-    <Stack>
-      <Input type="file" onChange={handleUpload} />
-      <DataTable headers={["address", "status", "tags"]}>
-        {addresses.map((address) => (
-          <Tr key={address.address}>
+    <Stack spacing={8}>
+      <Heading>Whitelist</Heading>
+      <DataTable headers={["Address", "Tags", "Status", "Modify"]}>
+        {addresses.map((address, index) => (
+          <Tr key={index}>
             <Td>{address.address}</Td>
-            <Td>{address.status === "" ? "Not set" : address.status}</Td>
             <Td> placeholder for tags... </Td>
+            <Td>
+              <Select
+                placeholder="Not Set"
+                value={address.status}
+                onChange={(e) => handleUpdateStatus(index, e.target.value)}
+              >
+                <option value="shortlisted">Shortlisted</option>
+                <option value="rejected">Rejected</option>
+                <option value="considering">Considering</option>
+              </Select>
+            </Td>
+            <Td>
+              <IconButton
+                onClick={() => handleDeleteAddress(index)}
+                aria-label="delete"
+                icon={<Icon as={MdDelete} />}
+              />
+            </Td>
           </Tr>
         ))}
       </DataTable>
-      <Input
-        placeholder="Address"
-        value={newAddress}
-        onChange={(e) => setNewAddress(e.target.value)}
-      />
-      <Button onClick={handleAddAdress}>Add Address</Button>
       <Button onClick={handleDownload}>Download List as CSV</Button>
+      <Stack>
+        <Heading size="lg">Add Address</Heading>
+        <Input
+          placeholder="Address"
+          value={newAddress}
+          onChange={(e) => setNewAddress(e.target.value)}
+        />
+        {hasAddressError && (
+          <Text color="red">Address must be at least 32 characters</Text>
+        )}
+        <Button onClick={handleAddAddress}>Add Address</Button>
+        <Input type="file" onChange={handleUpload} />
+      </Stack>
     </Stack>
   );
 }
